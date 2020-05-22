72707459c978119bbadb03f7f5bf91660e679cea
Mrgurmanas <Paulius543210@gmail.com>
2020-05-22 18:10:38 +0300

    CI 2.0

---
+++ Minesweeper/Assets/Editor/SomeGreenTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class SomeGreenTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void SomeGreenTestPassing()
+        {
+            Assert.Pass();
+        }
+    }
+}
